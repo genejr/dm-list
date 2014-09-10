3fe6cd336108648f045cadcad9fec113a2995f79
digilord <digilord@me.com>
2014-09-10 14:22:45 -0700

    Bumped version for publication on Troposphere.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A list and associated forms for managing list records",
-  version: "1.1.1",
+  version: "1.1.2",
   git: "https://github.com/digilord/dm-list",
   name: "digilord:list"
 });
