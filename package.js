25bc3b6ad049cf7ee149ca77173a0abe63c488ce
digilord <digilord@me.com>
2015-02-19 08:59:49 -0700

    Bumped version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A list and associated forms for managing list records",
-  version: "1.2.1",
+  version: "1.2.2",
   git: "https://github.com/digilord/dm-list",
   name: "digilord:list"
 });
